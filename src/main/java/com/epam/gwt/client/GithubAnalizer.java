0cd58f79f02fd8a92cf7c602e9d2ad87c834d407
Aleksandr <alexlukovkin@gmail.com>
2018-07-07 14:43:25 +0300

    dev add internationalization

--- src/main/java/com/epam/gwt/client/GithubAnalizer.java
+++ src/main/java/com/epam/gwt/client/GithubAnalizer.java
@@ -1,42 +1,23 @@
 package com.epam.gwt.client;
 
-import com.epam.gwt.shared.FieldVerifier;
+import com.epam.gwt.client.i18n.GithubAnalyzerConstants;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.i18n.client.LocaleInfo;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class GithubAnalizer implements EntryPoint {
-  /**
-   * The message displayed to the user when the server cannot be reached or
-   * returns an error.
-   */
-  private static final String SERVER_ERROR = "An error occurred while "
-      + "attempting to contact the server. Please check your network "
-      + "connection and try again.";
 
-  /**
-   * Create a remote service proxy to talk to the server-side Greeting service.
-   */
-  private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+  private final GithubAnalyzerConstants constants = GWT.create(GithubAnalyzerConstants.class);
 
-  /**
-   * This is the entry point method.
-   */
   public void onModuleLoad() {
     TextBox userNameField = new TextBox();
     PasswordTextBox passwordField = new PasswordTextBox();
-    Button loginButton = new Button();
-    loginButton.setText("Login");
+    Button loginButton = new Button(constants.loginButtonText());
 
     VerticalPanel verticalPanel = new VerticalPanel();
     verticalPanel.add(userNameField);
@@ -44,13 +25,14 @@ public class GithubAnalizer implements EntryPoint {
     verticalPanel.add(loginButton);
 
     DialogBox dialogBox = new DialogBox();
-    dialogBox.setText("Login to server");
+    dialogBox.setText(constants.dialogBoxTitle());
     dialogBox.setAnimationEnabled(true);
     dialogBox.add(verticalPanel);
 
     loginButton.addClickHandler(clickEvent -> {
       dialogBox.hide();
-      Window.alert("Successful login " + userNameField.getValue()  + " " + passwordField.getValue());
+      String locale = LocaleInfo.getCurrentLocale().getLocaleName();
+      Window.alert("Successful login: " + userNameField.getValue() + " "+ locale);
     });
 
     RootPanel.get().add(dialogBox);
